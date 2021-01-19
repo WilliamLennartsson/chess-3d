8d93b9ea0ac5bb7b3707a66e233280e50a0aac01
WilliamLennartsson <william.j.lennartsson@gmail.com>
2021-01-19 09:24:03 +0100

    chess html

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import chessApp from './chess/app'
+
+const menuBtn1 = document.querySelector('#menuBtn1')
+console.log('menuBtn1 :>> ', menuBtn1);
+menuBtn1.onclick = () => { window.location = "/chess.html" }
 window.onload = () => { chessApp() }
